0da48b6b591409a3d9a7cd4c0d0c46b460ea37c4
Khais COLIN <logistic-bot@protonmail.com>
2021-03-31 16:52:24 +0200

    Run tests and exit correctly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![warn(missing_docs)]
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 //! This module contains the entry point for the kernel.
 
@@ -15,9 +18,32 @@ use core::panic::PanicInfo;
 pub extern "C" fn _start() -> ! {
     println!("Hello world!");
 
-    panic!("End of kernel!");
+    #[cfg(test)]
+    test_main();
 
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+/// Exit code for qemu. This is primarly used for unit testing.
+pub enum QemuExitCode {
+    /// No problem, all test passed.
+    Success = 0x10,
+    /// Some problem, some test failed.
+    Failed = 0x11,
+}
+
+/// Exit qemu (thus terminating the kernel) with the specified exit code.
+///
+/// A suitable debug-exit device is expected on port 0xf4.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 #[panic_handler]
@@ -26,4 +52,21 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    println!("Ran {} tests", tests.len());
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
