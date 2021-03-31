09560f55b38cfadfab27394fdb7ff6adc896e388
Khais COLIN <logistic-bot@protonmail.com>
2021-03-31 17:43:31 +0200

    A few tests for vga_buffer

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -150,3 +150,20 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_many() {
+    for i in 0..200 {
+        println!("test_println_many output #{}", i);
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read(); // If the implementation changed, delete this test
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
