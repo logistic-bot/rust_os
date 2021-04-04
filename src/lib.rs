2c19d8f3fdff33f8423647224fa211e440e58a2b
Khais COLIN <logistic-bot@protonmail.com>
2021-04-04 23:10:50 +0200

    No Harness tests

---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+pub mod serial;
+pub mod vga_buffer;
+
+/// This trait marks a function as testable. It is used for testing
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+
+    for test in tests {
+        test.run();
+    }
+
+    serial_println!("Ran {} tests", tests.len());
+    exit_qemu(QemuExitCode::Success);
+}
+
+/// This function is called on panic, and prints the panic information to the vga text buffer and the serial interface
+///
+/// It also exits qemu with failure
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]");
+    println!("{}", info);
+    serial_println!("{}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+/// Exit code for qemu. This is primarly used for unit testing.
+pub enum QemuExitCode {
+    /// No problem, all test passed.
+    Success = 0x10,
+    /// Some problem, some test failed.
+    Failed = 0x11,
+}
+
+/// Exit qemu (thus terminating the kernel) with the specified exit code.
+///
+/// A suitable debug-exit device is expected on port 0xf4.
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
