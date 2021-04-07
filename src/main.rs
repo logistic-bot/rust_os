778386b3db4cbe4c109278eda32990161d26614d
Khais COLIN <logistic-bot@protonmail.com>
2021-04-07 21:10:48 +0200

    Trigger stack overflow

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,11 @@ pub extern "C" fn _start() -> ! {
 
     rust_os::init();
 
-    unsafe {
-        *(0xdeadc0de as *mut u64) = 42;
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
