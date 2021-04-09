03cae182fe01448fbd6bbd87413da4cbf37f90e1
Khais COLIN <logistic-bot@protonmail.com>
2021-04-10 01:19:06 +0200

    Print scancodes of PS2 keyboard

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -50,7 +50,11 @@ pub fn init_idt() {
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-    print!("k");
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe { port.read() };
+    printl("{}", scancode);
 
     unsafe {
         PICS.lock()
